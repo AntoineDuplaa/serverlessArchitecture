2596b70331692c6d3d3519b418dc5b0dd71c69ee
HugoMangematin <71603466+HugoMangematin@users.noreply.github.com>
2022-05-30 10:10:45 +0200

    update account

--- src/routes/Account.js
+++ src/routes/Account.js
@@ -1,10 +1,14 @@
 import {useEffect, useState} from "react";
-import {doc, getDoc} from "firebase/firestore";
+import {doc, getDoc, setDoc, updateDoc} from "firebase/firestore";
+import {updateEmail} from "firebase/auth";
 import {auth, db} from "../firebase";
 
 
 export const Account = () => {
   const [userData, setUserData] = useState({});
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   useEffect( () => {
     const getData = async () => {
@@ -17,10 +21,41 @@ export const Account = () => {
     getData();
   },[])
 
+    const handleUpdate = async () => {
+        await updateEmail(auth.currentUser, email);
+        const userDocRef = doc(db, "Users", auth.currentUser.uid);
+        await updateDoc(userDocRef, {
+            email: email,
+            firstName: firstName,
+            lastName: lastName
+        })
+    }
 
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handleChangeFirstName = (event) => {
+        setFirstName(event.target.value);
+    }
+
+    const handleChangeLastName = (event) => {
+        setLastName(event.target.value);
+    }
 
   return (
     <div>
+        <h1>{userData.email}</h1>
+        <h2>{userData.firstName}</h2>
+        <h2>{userData.lastName}</h2>
+
+        <label>Email</label>
+        <input onChange={handleChangeEmail}/>
+        <label style={{marginTop: "10px"}}>First name</label>
+        <input onChange={handleChangeFirstName}/>
+        <label style={{marginTop: "10px"}}>Last name</label>
+        <input onChange={handleChangeLastName}/>
+        <button style={{marginTop: "10px"}} onClick={handleUpdate}>Edit</button>
       {/*<button onClick={() => deleteAccount}>Supprimer son compte</button>*/}
     </div>
   )
